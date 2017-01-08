83b88d24e009daf91217388c581dc5adb16593c3
Julie Morriseau <jmorriseau@email.neit.edu>
2017-01-07 19:45:24 -0500

    update status update section

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ function showSection(section,elem){
   $('.sub-container').removeClass('show-section');
   $('#' + section).removeClass('hide-section').addClass('show-section');
 }
+
+function toggleAccordian(elem){
+  $(elem).next(".accordian-body").slideToggle();
+  $(elem).parent().toggleClass("expanded");
+  if($(elem).parent().hasClass('expanded')){
+    $(elem).children("span").removeClass("fa-plus").addClass("fa-minus");
+  }
+  else{
+    $(elem).children("span").removeClass("fa-minus").addClass("fa-plus");
+  }
+}
